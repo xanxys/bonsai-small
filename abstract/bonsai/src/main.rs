ac46c0f56a9d0fdbb03e26bfe475e13cbeb5b2d4
xanxys <xanxys@gmail.com>
2017-02-11 16:23:50 +0900

    Add empty OpenGL window.

--- abstract/bonsai/src/main.rs
+++ abstract/bonsai/src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate glium;
+
 use std::collections::HashMap;
 
 #[derive(Debug, Copy, Clone)]
@@ -92,6 +95,22 @@ fn main() {
             regs: [0; 4],
         });
         w.next_id += 1;
+    }
+
+    use glium::{DisplayBuild, Surface};
+    let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
+    loop {
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.finish().unwrap();
+
+        // listing the events produced by the window and waiting to be received
+        for ev in display.poll_events() {
+            match ev {
+                glium::glutin::Event::Closed => return,   // the window has been closed by the user
+                _ => ()
+            }
+        }
     }
 
     for _ in 0i64..10 {
