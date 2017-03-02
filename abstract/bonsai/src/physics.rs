e370f19faf2ab49541f869be6dd9985c06daa936
xanxys <xanxys@gmail.com>
2017-03-02 22:10:25 +0900

    Partially implement cell division.

--- abstract/bonsai/src/physics.rs
+++ abstract/bonsai/src/physics.rs
@@ -1,5 +1,6 @@
 use std::collections::HashSet;
 use ndarray::prelude::*;
+use std::cmp;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct V3(pub f64, pub f64, pub f64);
@@ -57,6 +58,8 @@ pub struct World {
 
     pub blocks: Array3<Block>,
     bedrocks: HashSet<I3>,
+
+    pub wind: V3,
 }
 
 
@@ -103,13 +106,19 @@ Operations:
         nearby E, Dd
 */
 
-fn step_code(c: &mut Cell){
+enum StateDelta {
+    NoChange,
+    KillSelf,
+    AddCell(Cell),
+}
+
+fn step_code(c: &mut Cell, occupation: &HashSet<I3>, blocks: &Array3<Block>) -> StateDelta {
     if c.epsilon == 0 {
         c.decay += 1;
         if c.decay == 0xff {
-            // Delete cell.
+            return StateDelta::KillSelf;
         }
-        return;
+        return StateDelta::NoChange;
     }
     // Execute the instruction.
     c.decay = 0;
@@ -119,95 +128,140 @@ fn step_code(c: &mut Cell){
     let dst = (inst & 3) as usize;
     let src = ((inst >> 2) & 3) as usize;
 
-    if inst < 0x04 {
-        // divide
-    } else if inst < 0x08 {
-        // check
-    } else if inst < 0x0c {
-        // share * 2
-    } else if inst < 0x14 {
-        // force * 2
-    } else if inst < 0x1c {
-        // fuse
+    let mut st_delta = StateDelta::NoChange;
+    if inst < 0x04 { // divide
+        let mut target = None;
+        if c.ext && dst != 0 {
+            let btypes = [Block::Bedrock, Block::Soil, Block::Water, Block::Air];
+            let btarget = btypes[dst];
+
+            let I3(ix, iy, iz) = c.pi;
+            'nsearch: for x in cmp::max(ix-1, 0)..cmp::min(ix+2, HSIZE as i16) {
+                for y in cmp::max(iy-1, 0)..cmp::min(iy+2, HSIZE as i16) {
+                    for z in cmp::max(iz-1, 0)..cmp::min(iz+2, VSIZE as i16) {
+                        let i = I3(x, y, z);
+                        if blocks[(x as usize, y as usize, z as usize)] == btarget && !occupation.contains(&i) && i != c.pi {
+                            target = Some(i);
+                            break 'nsearch;
+                        }
+                    }
+                }
+            }
+        }
+
+        match target {
+            Some(i) => {
+                let I3(x,y,z) = i;
+                let p = V3(x as f64 + 0.5, y as f64 + 0.5, z as f64 + 0.5);
+                let eps_remain = c.epsilon / 2;
+                let eps_new_cell = c.epsilon - eps_remain;
+
+                let mut new_cell = Cell{
+                    id: 0,
+
+                    p: p,
+                    pi: i,
+                    dp: c.dp,
+
+                    epsilon: eps_new_cell,
+                    decay:0,
+                    prog: [0; 256],
+                    regs: [0; 4],
+
+                    ip: 0,
+                    ext: false,
+                    result: false,
+                };
+                for i in 0..128 {
+                    new_cell.prog[i] = c.prog[128 + i];
+                    c.prog[128 + i] = 0;
+                }
+                st_delta = StateDelta::AddCell(new_cell);
+                c.result = true;
+            },
+            None => {
+                c.result = false;
+            }
+        }
+        c.ext = false;
+    } else if inst < 0x08 { // check
+    } else if inst < 0x0c { // share * 2
+    } else if inst < 0x14 { // force * 2
+    } else if inst < 0x1c { // fuse
         c.ext = true;
-    } else if inst < 0x20 {
-        // RESERVED.
-    } else if inst < 0x21 {
+    } else if inst < 0x20 { // RESERVED.
+    } else if inst < 0x21 { // drain
         c.epsilon = 0;
-    } else if inst < 0x22 {
+    } else if inst < 0x22 { // clone
         for i in 0..128 {
             c.prog[i+128] = c.prog[i];
         }
         c.ext = true;
-    } else if inst < 0x23 {
+    } else if inst < 0x23 { // reduce
         for i in 128..256 {
             c.prog[i] = 0;
         }
         c.ext = false;
-    } else if inst < 0x24 {
+    } else if inst < 0x24 { // flip
         c.result = !c.result;
-    } else if inst < 0x25 {
-        // get-alpha
-    } else if inst < 0x26 {
-        // get-phi
-    } else if inst < 0x20 {
-        // RESERVED
-    } else if inst < 0x30 {
+    } else if inst < 0x25 { // get-alpha
+    } else if inst < 0x26 { // get-phi
+    } else if inst < 0x20 { // RESERVED
+    } else if inst < 0x30 { // jmpa
         if src & 1 == 0 || c.result {
             c.ip = c.regs[dst];
-            return;
+            return StateDelta::NoChange;
         }
-    } else if inst < 0x3f {
+    } else if inst < 0x3f { // jmpr
         if src & 1 == 0 || c.result {
             c.ip += c.regs[dst];
-            return;
+            return StateDelta::NoChange;
         }
-    } else if inst < 0x40 {
+    } else if inst < 0x40 { // jmpc
         if src & 1 == 0 || c.result {
             for i in 1..128 {
                 let addr = (c.ip + i) & 0x7f;
                 if c.prog[addr as usize] == c.regs[dst] {
                     c.ip = addr;
                     c.result = true;
-                    return;
+                    return StateDelta::NoChange;
                 }
             }
             c.result = false;
-            return;
+            return StateDelta::NoChange;
         }
-    } else if inst < 0x80 {
+    } else if inst < 0x80 { // movi
         c.regs[dst] = (inst >> 2) & 0xf;
     // Upper half: [0x80, 0xff]
-    } else if inst < 0x84 {
+    } else if inst < 0x84 { // inspect
         c.regs[dst] = c.epsilon;
-    } else if inst < 0x88 {
+    } else if inst < 0x88 { // not
         c.regs[dst] = !c.regs[dst];
-    } else if inst < 0x8c {
+    } else if inst < 0x8c { // swap
         let v = c.regs[dst];
         c.regs[dst] = (v << 4) | (v >> 4);
-    } else if inst < 0x90 {
-        // RESERVED.
-    } else if inst < 0xa0 {
+    } else if inst < 0x90 { // RESERVED.
+    } else if inst < 0xa0 { // and
         c.regs[dst] &= c.regs[src];
-    } else if inst < 0xb0 {
+    } else if inst < 0xb0 { // or
         c.regs[dst] |= c.regs[src];
-    } else if inst < 0xc0 {
+    } else if inst < 0xc0 { // add
         let sum = (c.regs[dst] as u16) + (c.regs[src] as u16);
         c.regs[dst] = (sum & 0xff) as u8;
         c.result = sum & 0x100 > 0;
-    } else if inst < 0xd0 {
+    } else if inst < 0xd0 { // mov
         c.regs[dst] = c.regs[src];
-    } else if inst < 0xe0 {
+    } else if inst < 0xe0 { // st
         let addr = if c.ext {c.regs[dst]} else {c.regs[dst] & 0x7f};
         c.prog[addr as usize] = c.regs[src];
-    } else if inst < 0xf0 {
+    } else if inst < 0xf0 { // ld
         let addr = if c.ext {c.regs[src]} else {c.regs[src] & 0x7f};
         c.regs[dst] = c.prog[addr as usize];
-    } else {
-        // Nearby
+    } else { // nearby
     }
     c.ip += 1;
     c.ip &= 0x7f;
+    return st_delta;
 }
 
 pub fn empty_world() -> World {
@@ -219,6 +273,8 @@ pub fn empty_world() -> World {
 
         blocks: Array::from_elem(BLOCKS_SHAPE, Block::Air),
         bedrocks: HashSet::new(),
+
+        wind: V3(0.0, 0.0, 0.0),
     };
 }
 
@@ -246,11 +302,17 @@ impl World {
             occupation.insert(cell.pi.clone());
         }
         for cell in &mut self.cells {
-            step_code(cell);
+            let st_delta = step_code(cell, &occupation, &self.blocks);
+            match st_delta {
+                StateDelta::NoChange => {},
+                StateDelta::AddCell(cell) => {},
+                StateDelta::KillSelf => {},
+            }
 
             cell.dp.2 -= gravity;
 
-            if self.blocks[(cell.pi.0 as usize, cell.pi.1 as usize, cell.pi.2 as usize)] == Block::Soil {
+            let block = self.blocks[(cell.pi.0 as usize, cell.pi.1 as usize, cell.pi.2 as usize)];
+            if block == Block::Soil {
                 if cell.p.0.fract() < 0.5 {
                     cell.dp.0 += stick;
                 } else {
